3f608f9db05b952ee178b5c146b2c88f9b74a296
k4rbon <128942271+k4rbon@users.noreply.github.com>
2023-03-26 01:17:43 +0100

    Add files via upload

---
+++ JusticeRP-Kox.lua
@@ -0,0 +1,21 @@
+StopResource('NeedRP.eu')
+
+local JusticeRP = false
+
+AddEventHandler('onResourceStop', function(resource)
+	if resource == GetCurrentResourceName() then
+		if GetResourceState('NeedRP.eu') == 'stopped' then
+			StartResource('JusticeRP')
+		end
+	end
+end)
+
+
+RegisterNetEvent('JusticeRP:Load')
+AddEventHandler('JusticeRP:Load', function(boolean)
+    if boolean ~= nil then
+        JusticeRP = boolean
+        print('Serwer JusticeRP załadowany.')
+    end
+end)
+
